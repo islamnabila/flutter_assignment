f6e8d6da30a4fd94f415666ed4a9c99da754e84a
islamnabila <81310064+islamnabila@users.noreply.github.com>
2023-09-30 17:38:04 +0600

    module8 assignment added

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,144 @@
-//module 5 assignment
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp( MyApp());
+void main() {
+  runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.green
-      ),
       home: MyHomePage(),
     );
   }
 }
-class MyHomePage extends StatelessWidget{
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
+    var _mediaQuery = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 100,
-        elevation: 70,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Icon(Icons.add_business),
-            Text("Home", style: TextStyle(fontSize: 25, color: Colors.white),),
-            Icon(Icons.search)
-          ],
-        ),
+        title: Text("Profile"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("This is mod 5 Assignment", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),),
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: RichText(text: TextSpan(
-                style: TextStyle(color: Colors.orange, fontSize: 25),
-               children: [
-                 TextSpan(text: "My", style: TextStyle(color: Colors.pink, fontSize: 23)),
-                 TextSpan(text: " phone",style: TextStyle(color: Colors.blue, fontSize: 15)),
-                 TextSpan(text: " name", style: TextStyle(color: Colors.purple, fontSize: 20)),
-                 TextSpan(text: " Poco X3")
+      body: LayoutBuilder(builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  height: _mediaQuery.height * 0.42,
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://expertphotography.b-cdn.net/wp-content/uploads/2020/07/social-media-for-photographers-follow-1.jpg"),
+                          fit: BoxFit.cover)),
+                ),
+              ),
+              Text(
+                "Jhon Doe",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              Expanded(
+                child: GridView.builder(
+                    itemCount: 9,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        mainAxisSpacing: 10,
+                        childAspectRatio: 1.10),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: EdgeInsets.only(left: 7, right: 7),
+                        child: Placeholder(
+                          child: Image.network("https://img.freepik.com/premium-photo/beautiful-landscape-based-3d-rendering-illustration_771975-25.jpg?w=2000",fit: BoxFit.cover,),
+                        ),
+                      );
+                    }),
+              )
+            ],
+          );
+        } else {
+          return Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  margin: EdgeInsets.only(right: 25),
+                  width: _mediaQuery.width * 0.42,
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://expertphotography.b-cdn.net/wp-content/uploads/2020/07/social-media-for-photographers-follow-1.jpg",),
+                          fit: BoxFit.cover)),
+                ),
+              ),
 
-               ]
-              ),
-              ),
-            )
-          ],
-        ),
-      ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Text(
+                        "Jhon Doe",
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Text(
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the ",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 15,),
+                    Expanded(
+                      child: GridView.builder(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.vertical,
+                        itemCount: 9,
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            mainAxisSpacing: 10,
+                            childAspectRatio: 1.10
+
+                          ), itemBuilder: (context, index){
+                            return Container(
+                              margin: EdgeInsets.only(right: 10),
+                              child: Placeholder(
+                                child: Image.network("https://img.freepik.com/premium-photo/beautiful-landscape-based-3d-rendering-illustration_771975-25.jpg?w=2000",fit: BoxFit.cover,),
+                              ),
+                            );
+                      }),
+                    )
+
+                  ],
+                ),
+              )
+            ],
+          );
+        }
+      }),
     );
   }
 }
+
+
