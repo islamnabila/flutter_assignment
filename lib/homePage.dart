80c7bf7e558718a30f3a0da17c861d455cc7d825
islamnabila <81310064+islamnabila@users.noreply.github.com>
2023-10-07 20:38:45 +0600

    module 9 assignment added

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -12,7 +12,7 @@ class _MyHomePageState extends State<MyHomePage> {
   List photo = [
     {
       "img":
-          "https://sassafras.in/cdn/shop/products/SFTOPS40908-1.jpg?v=1666420733",
+      "https://sassafras.in/cdn/shop/products/SFTOPS40908-1.jpg?v=1666420733",
       "title": "Pullover",
       "color": "Black",
       "Size": "L",
@@ -21,7 +21,7 @@ class _MyHomePageState extends State<MyHomePage> {
     },
     {
       "img":
-          "https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/7/e/tr:w-270,/7eecdb0RIGO102_4.jpg?rnd=20200526195200",
+      "https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/7/e/tr:w-270,/7eecdb0RIGO102_4.jpg?rnd=20200526195200",
       "title": "T-Shirt",
       "color": "Gray",
       "Size": "L",
@@ -30,7 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
     },
     {
       "img":
-          "https://www.yellowclothing.net/cdn/shop/files/U6A0123_1024x1024.jpg?v=1691476403",
+      "https://www.yellowclothing.net/cdn/shop/files/U6A0123_1024x1024.jpg?v=1691476403",
       "title": "Sport dress",
       "color": "Black",
       "Size": "M",
@@ -49,8 +49,8 @@ class _MyHomePageState extends State<MyHomePage> {
         elevation: 0,
         actions: [
           Padding(
-            padding: const EdgeInsets.only(top: 10,right: 10),
-            child: Icon(Icons.search,color: Colors.black,),
+            padding: const EdgeInsets.only(top: 10, right: 10),
+            child: Icon(Icons.search, color: Colors.black),
           )
         ],
       ),
@@ -59,44 +59,52 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.only(bottom: 20,left: 10),
-              child: Text("My Bag",style: TextStyle(color:
-              Colors.black,fontSize: 30,fontWeight: FontWeight.bold),),
+              padding: const EdgeInsets.only(bottom: 20, left: 10),
+              child: Text(
+                "My Bag",
+                style: TextStyle(
+                    color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
+              ),
             ),
             Expanded(
-              flex: 3,
+              flex: 5,
               child: ListView.builder(
                 itemCount: photo.length,
                 itemBuilder: (context, index) {
                   int itemTotal =
-                      (int.parse(photo[index]["Price"]) * photo[index]["count"])
-                          .toInt();
+                  (int.parse(photo[index]["Price"]) * photo[index]["count"])
+                      .toInt();
                   return Container(
                     margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    // padding: EdgeInsets.symmetric(horizontal: 10),
                     height: 110,
                     decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              blurRadius: 5,
-                              spreadRadius: 1)
-                        ]),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          blurRadius: 5,
+                          spreadRadius: 1,
+                        )
+                      ],
+                    ),
                     child: Row(
                       children: [
                         Container(
                           child: AspectRatio(
-                              aspectRatio: 16 / 16,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10),bottomLeft: Radius.circular(10)),
-                                  image: DecorationImage(
-                                    image: NetworkImage(photo[index]["img"],),fit: BoxFit.cover
-                                  )
+                            aspectRatio: 16 / 16,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  bottomLeft: Radius.circular(10),
                                 ),
-                              )
+                                image: DecorationImage(
+                                  image: NetworkImage(photo[index]["img"]),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
                           ),
                         ),
                         SizedBox(
@@ -104,43 +112,33 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                         Padding(
                           padding:
-                              const EdgeInsets.only(left: 5, top: 8, bottom: 8),
+                          const EdgeInsets.only(left: 5, top: 8, bottom: 8),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
                                 photo[index]["title"],
-                                style: Theme.of(context).textTheme.titleMedium,
+                                style: Theme.of(context).textTheme.subtitle1,
                               ),
                               Row(
                                 children: [
-                                  RichText(
-                                    text: TextSpan(
-                                        text: "Colors: ",
-                                        style: TextStyle(
-                                            fontSize: 10, color: Colors.black),
-                                        children: [
-                                          TextSpan(
-                                              text: "${photo[index]["color"]}",
-                                              style:
-                                                  TextStyle(color: Colors.grey))
-                                        ]),
+                                  Text(
+                                    "Colors: ${photo[index]["color"]}",
+                                    style: TextStyle(
+                                      fontSize: 10,
+                                      color: Colors.black,
+                                    ),
                                   ),
                                   SizedBox(
                                     width: 16,
                                   ),
-                                  RichText(
-                                    text: TextSpan(
-                                        text: "Size: ",
-                                        style: TextStyle(
-                                            fontSize: 10, color: Colors.black),
-                                        children: [
-                                          TextSpan(
-                                              text: "${photo[index]["Size"]}",
-                                              style:
-                                                  TextStyle(color: Colors.grey))
-                                        ]),
-                                  )
+                                  Text(
+                                    "Size: ${photo[index]["Size"]}",
+                                    style: TextStyle(
+                                      fontSize: 10,
+                                      color: Colors.black,
+                                    ),
+                                  ),
                                 ],
                               ),
                               SizedBox(
@@ -152,25 +150,28 @@ class _MyHomePageState extends State<MyHomePage> {
                                     width: 32,
                                     height: 32,
                                     decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        shape: BoxShape.circle,
-                                        boxShadow: [
-                                          BoxShadow(
-                                              color: Colors.grey.withOpacity(0.4),
-                                              spreadRadius: 1,
-                                              blurRadius: 5)
-                                        ]),
+                                      color: Colors.white,
+                                      shape: BoxShape.circle,
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.4),
+                                          spreadRadius: 1,
+                                          blurRadius: 5,
+                                        )
+                                      ],
+                                    ),
                                     child: InkWell(
-                                        onTap: () {
-                                          setState(() {
-                                            if (photo[index]["count"] > 0) {
-                                              photo[index]["count"]--;
-                                              totalAmount -= int.parse(
-                                                  photo[index]["Price"]);
-                                            }
-                                          });
-                                        },
-                                        child: Icon(Icons.remove)),
+                                      onTap: () {
+                                        setState(() {
+                                          if (photo[index]["count"] > 0) {
+                                            photo[index]["count"]--;
+                                            totalAmount -=
+                                                int.parse(photo[index]["Price"]);
+                                          }
+                                        });
+                                      },
+                                      child: Icon(Icons.remove),
+                                    ),
                                   ),
                                   SizedBox(
                                     width: 18,
@@ -185,26 +186,29 @@ class _MyHomePageState extends State<MyHomePage> {
                                     width: 32,
                                     height: 32,
                                     decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        shape: BoxShape.circle,
-                                        boxShadow: [
-                                          BoxShadow(
-                                              color: Colors.grey.withOpacity(0.4),
-                                              spreadRadius: 1,
-                                              blurRadius: 5)
-                                        ]),
+                                      color: Colors.white,
+                                      shape: BoxShape.circle,
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.4),
+                                          spreadRadius: 1,
+                                          blurRadius: 5,
+                                        )
+                                      ],
+                                    ),
                                     child: InkWell(
-                                        onTap: () {
-                                          setState(() {
-                                            photo[index]["count"]++;
-                                            totalAmount +=
-                                                int.parse(photo[index]["Price"]);
-                                          });
-                                        },
-                                        child: Icon(Icons.add)),
-                                  )
+                                      onTap: () {
+                                        setState(() {
+                                          photo[index]["count"]++;
+                                          totalAmount +=
+                                              int.parse(photo[index]["Price"]);
+                                        });
+                                      },
+                                      child: Icon(Icons.add),
+                                    ),
+                                  ),
                                 ],
-                              )
+                              ),
                             ],
                           ),
                         ),
@@ -254,22 +258,29 @@ class _MyHomePageState extends State<MyHomePage> {
             Padding(
               padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
               child: SizedBox(
-                  width: double.infinity,
-                  height: 45,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.red,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      onPressed: () {
-                        final message = SnackBar(content: Text("Congratulation"));
-                        ScaffoldMessenger.of(context).showSnackBar(message);
-                      },
-                      child: Text(
-                        "CHECK OUT",
-                        style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ))),
+                width: double.infinity,
+                height: 45,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    final message = SnackBar(content: Text("Congratulation"));
+                    ScaffoldMessenger.of(context).showSnackBar(message);
+                  },
+                  child: Text(
+                    "CHECK OUT",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             )
           ],
         ),
