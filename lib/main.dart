f574ac96a973a45294c048f5f96b5a60450e65db
islamnabila <81310064+islamnabila@users.noreply.github.com>
2023-11-03 20:39:29 +0600

    module 11 assignment added

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,19 @@
-//module 5 assignment
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_assignment/screen/photo_gallery_list_screen.dart';
 
 void main(){
-  runApp( MyApp());
+  runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.green
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-class MyHomePage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 100,
-        elevation: 70,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Icon(Icons.add_business),
-            Text("Home", style: TextStyle(fontSize: 25, color: Colors.white),),
-            Icon(Icons.search)
-          ],
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("This is mod 5 Assignment", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),),
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: RichText(text: TextSpan(
-                style: TextStyle(color: Colors.orange, fontSize: 25),
-               children: [
-                 TextSpan(text: "My", style: TextStyle(color: Colors.pink, fontSize: 23)),
-                 TextSpan(text: " phone",style: TextStyle(color: Colors.blue, fontSize: 15)),
-                 TextSpan(text: " name", style: TextStyle(color: Colors.purple, fontSize: 20)),
-                 TextSpan(text: " Poco X3")
-
-               ]
-              ),
-              ),
-            )
-          ],
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: PhotoGalleryListScreen(),
     );
   }
 }
